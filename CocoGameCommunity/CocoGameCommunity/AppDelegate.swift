c64354db9238e51ef2c0ffc444397dd52a989692
student <student@iMac-54.local>
2018-12-26 11:30:52 +0800

    add view

--- CocoGameCommunity/CocoGameCommunity/AppDelegate.swift
+++ CocoGameCommunity/CocoGameCommunity/AppDelegate.swift
@@ -17,31 +17,38 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        // 创建窗口
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.makeKeyAndVisible()
+        
+        // 创建视图
+        let community = CommunityViewController()
+        let gamelibrary = GameLibraryViewController()
+        let friend = FriendViewController()
+        let personalCenter = PersonalCenterViewController()
+        
+        // 设置视图属性
+        community.tabBarItem = UITabBarItem(title: "论坛", image: nil, selectedImage: nil)
+        community.title = "论坛"
+        gamelibrary.tabBarItem = UITabBarItem(title: "游戏库", image: nil, selectedImage: nil)
+        gamelibrary.title = "游戏库"
+        friend.tabBarItem = UITabBarItem(title: "好友", image: nil, selectedImage: nil)
+        personalCenter.tabBarItem = UITabBarItem(title: "个人中心", image: nil, selectedImage: nil)
+        
+        // 创建导航控制器
+        let navCommunity = UINavigationController(rootViewController: community)
+        let navGameLibrary = UINavigationController(rootViewController: gamelibrary)
+        let navFriend = UINavigationController(rootViewController: friend)
+        let navPersonalCenter = UINavigationController(rootViewController: personalCenter)
+        
+        // 创建分栏控制器
+        let tabber = UITabBarController()
+        tabber.viewControllers = [navCommunity, navGameLibrary, navFriend, navPersonalCenter]
+        
+        window?.rootViewController = tabber
+        
         return true
-    }
-
-    func applicationWillResignActive(_ application: UIApplication) {
-        // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-        // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
-    }
-
-    func applicationDidEnterBackground(_ application: UIApplication) {
-        // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
-        // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
-    }
-
-    func applicationWillEnterForeground(_ application: UIApplication) {
-        // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
-    }
-
-    func applicationDidBecomeActive(_ application: UIApplication) {
-        // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
-    }
-
-    func applicationWillTerminate(_ application: UIApplication) {
-        // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
-        // Saves changes in the application's managed object context before the application terminates.
-        self.saveContext()
     }
 
     // MARK: - Core Data stack
