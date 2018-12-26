a8291fd5fbdf24796a65213d8a75151161ce1ec0
student <student@iMac-54.local>
2018-12-26 09:18:44 +0800

    create Project

---
+++ CocoGameCommunity/CocoGameCommunity/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CocoGameCommunity
+//
+//  Created by student on 2018/12/26.
+//  Copyright © 2018年 余鑫. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
